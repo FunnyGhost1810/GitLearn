a4061b646bd2b994a76490500926d51e484608bb
FunnyGhost1810 <funnyghost328@gmail.com>
2022-01-17 17:06:28 +0200

    add change to GitLearn

--- src/gitLearn/GitLearn.java
+++ src/gitLearn/GitLearn.java
@@ -2,6 +2,7 @@ package gitLearn;
 
 public class GitLearn {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git! Change");
+
     }
 }
